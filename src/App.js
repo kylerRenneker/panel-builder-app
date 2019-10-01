2df6f65882d009ff60da7c343da718cc97fb72f1
Renneker <kyler.renneker@atos.net>
2019-10-01 16:26:48 -0400

    set up base folder structure and added sample nav component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React from 'react';
-import './App.css';
+import { Route, Switch } from 'react-router-dom';
+import Header from './components/Header/Header'
+import LandingPage from './routes/LandingPage/LandingPage';
 
 function App() {
   return (
     <main className="App">
+      <Header />
+
+      <Switch>
+        <Route path='/' component={LandingPage} />
+      </Switch>
+
     </main>
   );
 }
---
+++ src/components/Header/Header.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Header() {
+    return (
+        <nav>
+            <span>This is the navbar</span>
+        </nav>
+    )
+}
